3843973913b747a927adc259b347235bbfb73d7a
manijoshi <65389710+manijoshi@users.noreply.github.com>
2023-10-29 18:07:51 +0000

    03_conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let name = "Malti";
+let age = 5;
+let maxDaysCount = "12a";//output=> NaN and type=>number
+
+
+
+let numberMaxDaysCount = Number(maxDaysCount);
+// console.log(numberMaxDaysCount);
+// console.log(typeof numberMaxDaysCount);
+
+let ageString = String(age);
+// console.log(ageString);
+// console.log(typeof ageString);
+
+let booleanAge = Boolean(age);
+// console.log(booleanAge);
+// console.log(typeof booleanAge);
+console.log(2/3);
+console.log(1+"2");
+console.log(1+"2"+1);
+console.log(1+"2"+1/2);
+console.log(+false);
+console.log(+true);
+console.log(+"");
+console.log(+null);
+console.log(true-false);
+
+console.log(age++);
+console.log(age+1);
