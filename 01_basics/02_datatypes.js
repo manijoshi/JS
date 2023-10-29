700615a81c1922825e9c9c0c870dfe0d80aced94
manijoshi <65389710+manijoshi@users.noreply.github.com>
2023-10-29 18:41:57 +0000

    datatype summary

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,7 +4,12 @@ let name = "Merry";
 let isMarried = false;
 let graduated = null;
 let isWorking;
+let a = Symbol('122');
+let b = Symbol('122');
+console.log(a==b);
+console.table([typeof age,typeof name,typeof isMarried, typeof graduated, typeof isWorking]);
 /*
+Primittive/Call By Value Types:-
 number => 2 to the power 53
 bigint
 string => ""
@@ -13,5 +18,17 @@ null => standalone value
 undefined => undefined
 symbol => define uniqueness
 
+Non-Primittive/Referencing Types:-
+Array
+Object,
+Function
 */
-console.table([typeof age,typeof name,typeof isMarried, typeof graduated, typeof isWorking]);
+
+const empNames = ["John","Malti","NinjaD","Michale"];
+const myObj = {name:"teepu",
+age:95,
+generation:"The Silent Generation"};
+const greet = function(){
+    console.log("Hello World");
+}
+console.table([typeof empNames,typeof(myObj),typeof greet]);
