e4a0236fc24450e8bc41dc8f0f38f13c69a23168
manijoshi <65389710+manijoshi@users.noreply.github.com>
2023-10-29 14:53:02 +0000

    02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // use all JS code as newer version of js
+let age = 98;
+let name = "Merry";
+let isMarried = false;
+let graduated = null;
+let isWorking;
+/*
+number => 2 to the power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => undefined
+symbol => define uniqueness
+
+*/
+console.table([typeof age,typeof name,typeof isMarried, typeof graduated, typeof isWorking]);
