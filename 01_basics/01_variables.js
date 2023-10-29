5c216db44accc16f6e20eaff4b84cb9d2e6ad9fb
manijoshi <65389710+manijoshi@users.noreply.github.com>
2023-10-29 14:25:26 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1234;
+let accountEmail = "john@test.com";
+var accountPassword = "XX1234";
+accountCity = "Jaipur";
+let accountState;
+//accountId= 3345; //can't be done
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
