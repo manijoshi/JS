ff20054af5def2f56eae86c6043544259f4bb942
manijoshi <65389710+manijoshi@users.noreply.github.com>
2023-10-29 18:18:49 +0000

    04_comparissions

---
+++ 01_basics/04_comparssions.js
@@ -0,0 +1,9 @@
+// console.log("02">1);
+console.log(null>0);
+console.log(null>null);
+console.log(null<1);
+console.log(null>=0);//works different from > or <
+console.log("2"==2);
+console.log("2"===2);
+console.log("2"!=0);
+console.log("2"!==0);
