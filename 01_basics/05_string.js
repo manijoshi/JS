a9ba778b5bf874f1bd8ea3c3cf0344b7b4c2f218
manijoshi <65389710+manijoshi@users.noreply.github.com>
2023-11-01 02:57:31 +0000

    string functions

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -37,4 +37,15 @@ console.log(Number.MAX_SAFE_INTEGER+2==Number.MAX_SAFE_INTEGER+1);//Number.MAX_S
 console.log(String.fromCharCode(65,97,66));
 console.log("manisha".at(3));//accepts negative as well as positive indexes
 console.log("manisha".charAt(3));
-console.log("manisha".charCodeAt(1));
+const c=undefined;
+console.log("a".charCodeAt(1));
+console.log("manisha ".concat("joshi ","ji"));
+const namesList = ["anisha",'manisha','isha'];
+console.log(...namesList);
+console.log("the quick brown fox jumps over the little lazy dog!".endsWith("dog",50));
+console.log("the quick brown fox jumps over the little lazy dog!".includes('the'));
+const str ="dog is a pet animal. dog is most loveable pet."
+const idxOfFirstDog = str.indexOf("dog");
+console.log(str.indexOf("dog",idxOfFirstDog+1));//indexOf is case-sensitive
+console.log(str.lastIndexOf("dog"));
+console.log("manisha".substring(1,4))
